4d2187cfdfe9c9ec31d11068d54eee5e3e03a5d7
OMARNOUFAL544 <omarnofal544@gmail.com>
2024-12-29 21:56:31 +0300

    Initial commit

---
+++ javascript/javascript.js
@@ -0,0 +1,63 @@
+onload=function(){
+    
+let x = 1000;
+let y = 500;
+
+
+let output1 = x / y;
+
+document.writeln(output1 + "<br>");
+
+
+
+let z = 1000;
+let a = 500;
+
+let output2 = z - a;
+document.writeln(output2 + "<br>");
+
+let b = 1000;
+let c = 500;
+
+let output3 = b + c;
+document.writeln(output3 + "<br>");
+
+let d = 1000;
+let e = 500;
+let output4 = d * e;
+document.writeln(output4 + "<br>");
+
+let f = 9;
+let g = 7;
+let t = 2;
+let output5 = (f+g+t) /3;  
+document.writeln(output5 + "<br>");
+
+let h = 150;
+let i = 30;
+let first = (150 * 30) / 100;
+let result6 = 150 - first;
+document.writeln(result6 + "<br>");
+
+let j = 18;
+let n = 30;
+let m = 20;
+let output7 = j <m< n;
+document.writeln(output7 + "<br>");
+
+
+
+let num1 = 2;
+let num2 = 3;
+let output8 = (num1**num2);
+document.writeln(output8 + "<br>");
+
+
+
+let num3 = 4;
+let num4 = 10;
+let output9 = (num4%num3);
+document.writeln(output9 + "<br>");
+
+
+}
